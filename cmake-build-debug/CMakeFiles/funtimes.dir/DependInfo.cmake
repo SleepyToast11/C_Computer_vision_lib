425d5f68b1da38068358f7cfd2c7f6f53aa34ad1
jsparnaay <jesp2000@live.ca>
2023-04-16 01:11:40 -0400

    sync debug files

---
+++ cmake-build-debug/CMakeFiles/funtimes.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jsparnaay/CLionProjects/funTimes/assignments/assignment1.c" "CMakeFiles/funtimes.dir/assignments/assignment1.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment1.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/assignments/assignment2.c" "CMakeFiles/funtimes.dir/assignments/assignment2.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment2.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/assignments/assignment3.c" "CMakeFiles/funtimes.dir/assignments/assignment3.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment3.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/component_analysis.c" "CMakeFiles/funtimes.dir/component_analysis.c.o" "gcc" "CMakeFiles/funtimes.dir/component_analysis.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/component_helper.c" "CMakeFiles/funtimes.dir/component_helper.c.o" "gcc" "CMakeFiles/funtimes.dir/component_helper.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/component_labeling.c" "CMakeFiles/funtimes.dir/component_labeling.c.o" "gcc" "CMakeFiles/funtimes.dir/component_labeling.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/edgeDetector.c" "CMakeFiles/funtimes.dir/edgeDetector.c.o" "gcc" "CMakeFiles/funtimes.dir/edgeDetector.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/filters.c" "CMakeFiles/funtimes.dir/filters.c.o" "gcc" "CMakeFiles/funtimes.dir/filters.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/helperFun.c" "CMakeFiles/funtimes.dir/helperFun.c.o" "gcc" "CMakeFiles/funtimes.dir/helperFun.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/image_analysis.c" "CMakeFiles/funtimes.dir/image_analysis.c.o" "gcc" "CMakeFiles/funtimes.dir/image_analysis.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/kmean.c" "CMakeFiles/funtimes.dir/kmean.c.o" "gcc" "CMakeFiles/funtimes.dir/kmean.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/main.c" "CMakeFiles/funtimes.dir/main.c.o" "gcc" "CMakeFiles/funtimes.dir/main.c.o.d"
+  "/home/jsparnaay/CLionProjects/funTimes/utility.c" "CMakeFiles/funtimes.dir/utility.c.o" "gcc" "CMakeFiles/funtimes.dir/utility.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/funtimes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for funtimes.
+# This may be replaced when dependencies are built.
