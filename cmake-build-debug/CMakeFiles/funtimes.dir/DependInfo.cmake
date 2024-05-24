bd6c3f75157d746c0a73933de9d267b630a670c7
SleepyToast11 <jesp2000@live.ca>
2024-05-23 22:54:34 -0400

    Update gitignore

--- cmake-build-debug/CMakeFiles/funtimes.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/funtimes.dir/DependInfo.cmake
@@ -8,23 +8,23 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/jsparnaay/CLionProjects/funTimes/assignments/assignment1.c" "CMakeFiles/funtimes.dir/assignments/assignment1.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment1.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/assignments/assignment2.c" "CMakeFiles/funtimes.dir/assignments/assignment2.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment2.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/assignments/assignment3.c" "CMakeFiles/funtimes.dir/assignments/assignment3.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment3.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/component_analysis.c" "CMakeFiles/funtimes.dir/component_analysis.c.o" "gcc" "CMakeFiles/funtimes.dir/component_analysis.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/component_helper.c" "CMakeFiles/funtimes.dir/component_helper.c.o" "gcc" "CMakeFiles/funtimes.dir/component_helper.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/component_labeling.c" "CMakeFiles/funtimes.dir/component_labeling.c.o" "gcc" "CMakeFiles/funtimes.dir/component_labeling.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/edgeDetector.c" "CMakeFiles/funtimes.dir/edgeDetector.c.o" "gcc" "CMakeFiles/funtimes.dir/edgeDetector.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/filters.c" "CMakeFiles/funtimes.dir/filters.c.o" "gcc" "CMakeFiles/funtimes.dir/filters.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/helperFun.c" "CMakeFiles/funtimes.dir/helperFun.c.o" "gcc" "CMakeFiles/funtimes.dir/helperFun.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/image_analysis.c" "CMakeFiles/funtimes.dir/image_analysis.c.o" "gcc" "CMakeFiles/funtimes.dir/image_analysis.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/kmean.c" "CMakeFiles/funtimes.dir/kmean.c.o" "gcc" "CMakeFiles/funtimes.dir/kmean.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/main.c" "CMakeFiles/funtimes.dir/main.c.o" "gcc" "CMakeFiles/funtimes.dir/main.c.o.d"
-  "/home/jsparnaay/CLionProjects/funTimes/utility.c" "CMakeFiles/funtimes.dir/utility.c.o" "gcc" "CMakeFiles/funtimes.dir/utility.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/assignments/assignment1.c" "CMakeFiles/funtimes.dir/assignments/assignment1.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment1.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/assignments/assignment2.c" "CMakeFiles/funtimes.dir/assignments/assignment2.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment2.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/assignments/assignment3.c" "CMakeFiles/funtimes.dir/assignments/assignment3.c.o" "gcc" "CMakeFiles/funtimes.dir/assignments/assignment3.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/component_analysis.c" "CMakeFiles/funtimes.dir/component_analysis.c.o" "gcc" "CMakeFiles/funtimes.dir/component_analysis.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/component_helper.c" "CMakeFiles/funtimes.dir/component_helper.c.o" "gcc" "CMakeFiles/funtimes.dir/component_helper.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/component_labeling.c" "CMakeFiles/funtimes.dir/component_labeling.c.o" "gcc" "CMakeFiles/funtimes.dir/component_labeling.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/edgeDetector.c" "CMakeFiles/funtimes.dir/edgeDetector.c.o" "gcc" "CMakeFiles/funtimes.dir/edgeDetector.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/filters.c" "CMakeFiles/funtimes.dir/filters.c.o" "gcc" "CMakeFiles/funtimes.dir/filters.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/helperFun.c" "CMakeFiles/funtimes.dir/helperFun.c.o" "gcc" "CMakeFiles/funtimes.dir/helperFun.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/image_analysis.c" "CMakeFiles/funtimes.dir/image_analysis.c.o" "gcc" "CMakeFiles/funtimes.dir/image_analysis.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/kmean.c" "CMakeFiles/funtimes.dir/kmean.c.o" "gcc" "CMakeFiles/funtimes.dir/kmean.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/main.c" "CMakeFiles/funtimes.dir/main.c.o" "gcc" "CMakeFiles/funtimes.dir/main.c.o.d"
+  "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/utility.c" "CMakeFiles/funtimes.dir/utility.c.o" "gcc" "CMakeFiles/funtimes.dir/utility.c.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/jsparnaay/CLionProjects/funTimes
+# Install script for directory: /home/jsparnaay/CLionProjects/C_Computer_vision_lib
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/jsparnaay/CLionProjects/funTimes/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/jsparnaay/CLionProjects/C_Computer_vision_lib/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
